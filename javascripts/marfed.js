8457eb3426ec1369bc69f190e4132398f786d2ca
flaviaza <flavia.ibanez@gmail.com>
2014-09-30 15:10:27 -0300

    home WIPw

---
+++ javascripts/marfed.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  var $grid = $('#grid'),
+      $sizer = $grid.find('.shuffle__sizer');
+
+  $grid.shuffle({
+    itemSelector: '.picture-item',
+    sizer: $sizer
+  });
+})
