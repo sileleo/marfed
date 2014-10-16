f30ddf8a6c439001696f07f5fd206a6f77cc1525
Silvana Corvalán <silvanacorvalan@MacBook-Pro-de-Silvana.local>
2014-10-16 15:19:59 -0300

    add work from sileleo

--- javascripts/marfed.js
+++ javascripts/marfed.js
@@ -7,3 +7,20 @@ $(document).ready(function() {
     sizer: $sizer
   });
 })
+
+// Home - Login animation
+// ======================================================================
+
+$(document).on('click', 'a[data-toggle=tab]', function () {
+
+  var action = $(this).hasClass('login') ? 'removeClass'
+                       : 'addClass'
+
+    $('.login-box')[action]('hide');
+
+  var action = $(this).hasClass('contact') ? 'removeClass'
+                       : 'addClass'
+
+    $('.contact-box')[action]('hide');
+
+});
